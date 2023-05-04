1cf6115f80b5153dda09b90529aa5c484d38125d
ahmad al fajri <116804256+inven22@users.noreply.github.com>
2023-05-04 13:27:25 +0700

    Update lagi

--- main.cpp
+++ main.cpp
@@ -29,12 +29,3 @@ int main() {
 }
 
 
-// MODUL UNTUK MENGATUR POSISI LOADING
-void gotoxy(int x, int y) 
-{ 
-    COORD coord;
-    coord.X = x; 
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-// MODUL UNTUK MENGATUR POSISI LOADING - END
