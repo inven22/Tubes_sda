9805015809e96cda660aa87a529f2d75c2c631b6
ahmad al fajri <116804256+inven22@users.noreply.github.com>
2023-05-04 13:07:45 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -7,43 +7,21 @@
 #include "program.h"
 
 
-void gotoxy(int x, int y);
+
 // DEKLARASI MODUL - END
 
 int main() {
 	
 
 	// PROSES FITUR SPLASH SCREEN
-	int load = 0; // UNTUK MENGHITUNG JUMLAH TITIK
+	
 	
 	
 	
 
-	for( int i = 0; i <= 100; i++ ) //MENGKALKULASI PROSES DARI 0-100%
-	{
-		if(i % 10 == 0)
-		{
-			gotoxy(21 + 15 + load, 6);
-			printf(".");
-			load++;
-			if(load == 6) //MENGHAPUS SEMUA TITIK
-			{
-				load = 0; 
-				printf("\tTunggu sebentar lagi");
-			}
-		}
-		gotoxy(21 + 21, 6);
-		printf("%i",i);
-		for(int j = 0; j < 50; j++ )  
-		{
-			for(int l = 0;l < 100000;l++) 
-			{
-				printf(""); 
-			}		
-		}
-	}
 	
-	main_antrian(); 
+	
+	home(); 
 
 	system("cls");
 	printf("Selesai");
